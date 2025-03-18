465fd4d5ec8ec4629c9bb174c85374ba079e0f40
varmaaradhula <rkvarmaa@gmail.com>
2025-03-18 10:40:47 +0000

    added controller

---
+++ src/main/java/com/bliss/PA/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.bliss.PA.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
+    }
+}
